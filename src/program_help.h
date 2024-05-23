990cf91802bcabd47dd96fbe74b60fcadd23607e
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-05-23 16:47:49 -0300

    Mention the new supported targets

--- src/program_help.h
+++ src/program_help.h
@@ -6,7 +6,7 @@ This file is auto-generated. Use the ../tools/program_help.h.py tool to regenera
 #define PROGRAM_HELP_H
 
 #define PROGRAM_HELP \
-	"usage: kai [-h] [-v] -u URL [-k] [-A USER_AGENT] [-x URI] [--doh-url URL] [-e URL] [-S] [--select-media MEDIA] [--select-stream VARIANT_STREAM] [-c CONCURRENCY] -o FILENAME [--debug] [-r COUNT]\n" \
+	"usage: kai [-h] [-v] -u URL [-k] [-A USER_AGENT] [-x URI] [--doh-url URL] [-e URL] [-r COUNT] [--debug] [-S] [--select-media MEDIA] [--select-stream VARIANT_STREAM] [-c CONCURRENCY] -o FILENAME\n" \
 	"\n" \
 	"A command-line utility to download contents from M3U8 playlists.\n" \
 	"\n" \
@@ -17,10 +17,13 @@ This file is auto-generated. Use the ../tools/program_help.h.py tool to regenera
 	"  -k, --insecure        Disable SSL/TLS certificate validation.\n" \
 	"  -A USER_AGENT, --user-agent USER_AGENT\n" \
 	"                        Send a custom User-Agent header to server.\n" \
-	"  -x URI, --proxy URI   Proxy all network traffic through the specified proxy.\n" \
+	"  -x URI, --proxy URI   Proxy all network traffic through the specified proxy. Supports SOCKS(4,5) and also HTTP(S) proxies.\n" \
 	"  --doh-url URL         Forward all DNS requests to a custom DoH (DNS-over-HTTPS) server.\n" \
 	"  -e URL, --referer URL\n" \
 	"                        Send a custom Referer header to server.\n" \
+	"  -r COUNT, --retry COUNT\n" \
+	"                        Specify how many times a failed HTTP request should be retried. Defaults to 0 (no retries).\n" \
+	"  --debug               Enable verbose logging of network requests for debugging purposes.\n" \
 	"  -S, --list-streams    List all available streams of the M3U8 playlist.\n" \
 	"  --select-media MEDIA  Select which media stream to download. By default, no additional media streams are downloaded.\n" \
 	"  --select-stream VARIANT_STREAM\n" \
@@ -29,9 +32,6 @@ This file is auto-generated. Use the ../tools/program_help.h.py tool to regenera
 	"                        Specify how many media segments should be downloaded simultaneously. Defaults to 1.\n" \
 	"  -o FILENAME, --output FILENAME\n" \
 	"                        Specify the output file.\n" \
-	"  --debug               Enable verbose logging of network requests for debugging purposes.\n" \
-	"  -r COUNT, --retry COUNT\n" \
-	"                        Specify how many times a failed HTTP request should be retried. Defaults to 0 (no retries at all).\n" \
 	"\n" \
 	"Note, options that take an argument require a equal sign. E.g. --url=URL\n" \
 
