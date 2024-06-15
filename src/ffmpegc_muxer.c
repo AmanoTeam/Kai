b70428779444f626d4dd5fe34b8a7012729fb097
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-06-15 00:08:13 -0300

    Update to GCC 14.1.0

--- src/ffmpegc_muxer.c
+++ src/ffmpegc_muxer.c
@@ -20,6 +20,18 @@ static const char* const FFMPEG_DEFAULT_OUTPUT_FLAGS[] = {
 	"-fflags", "+bitexact+autobsf"
 };
 
+static const char* const FFMPEG_INPUT_RANGE[] = {
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+	"14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25",
+	"26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37",
+	"38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+	"50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61",
+	"62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73",
+	"74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85",
+	"86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97",
+	"98", "99", "100"
+};
+
 int ffmpegc_mux_streams(char* const* const sources, const char* const destination) {
 	
 	int err = M3U8ERR_SUCCESS;
@@ -45,7 +57,7 @@ int ffmpegc_mux_streams(char* const* const sources, const char* const destinatio
 	}
 	
 	argc += 1; /* <command> */
-	argc += index * 4; /* -i <input> */
+	argc += index * 6; /* -allowed_extensions <extension> -i <input> -map <position>*/
 	argc += 1; /* <output> */
 	
 	executable = find_exe("ffmpeg");
@@ -81,6 +93,15 @@ int ffmpegc_mux_streams(char* const* const sources, const char* const destinatio
 		
 		argv[argvpos++] = "-i";
 		argv[argvpos++] = (char*) item;
+	}
+	
+	argc = index;
+	
+	for (index = 0; index < argc; index++) {
+		const char* const position = FFMPEG_INPUT_RANGE[index];
+		
+		argv[argvpos++] = "-map";
+		argv[argvpos++] = (char*) position;
 	}
 	
 	for (index = 0; index < sizeof(FFMPEG_DEFAULT_OUTPUT_FLAGS) / sizeof(*FFMPEG_DEFAULT_OUTPUT_FLAGS); index++) {
