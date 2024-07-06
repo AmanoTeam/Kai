e45fc74ec60d94800078f1a813d8478c9d7c9c1b
Your Name <you@example.com>
2024-07-06 03:11:22 -0300

    a

--- src/m3u8.c
+++ src/m3u8.c
@@ -63,7 +63,7 @@ The longest M3U8 tag name is 'EXT-X-DISCONTINUITY-SEQUENCE', which contains
 */
 #define M3U8_MAX_TAG_NAME_LEN (28 + 1)
 
-#define M3U8_MAX_ITEM_VALUE_LEN (50 * 1)
+#define M3U8_MAX_ITEM_VALUE_LEN (128 * 5)
 
 #define M3U8_MAX_PLAYLIST_LEN ((1024 * 1024 * 5) + 1)
 
