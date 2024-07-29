3dd6f451bad4b71e2ed86fd40718c2b9c2fb1769
Your Name <you@example.com>
2024-07-29 02:43:15 -0300

    Update README

--- src/signals.c
+++ src/signals.c
@@ -4,18 +4,20 @@
 #include "signals.h"
 
 int signal_sethandler(
-	const int signal,
+	const int code,
 	SIGNAL_HANDLER_RETURN (*handler)(SIGNAL_HANDLER_ARGS)
 ) {
 	
 	#if defined(_WIN32)
-	
+		if (signal(code, handler) == SIG_ERR) {
+			return -1;
+		}
 	#else
 		const struct sigaction action = {
 			.sa_handler = handler
 		};
 		
-		if (sigaction(signal, &action, NULL) == -1) {
+		if (sigaction(code, &action, NULL) == -1) {
 			return -1;
 		}
 	#endif
